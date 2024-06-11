557665e8c00180ec33d58599d9effb6e2c0f0d7e
Surafelfs <Surafel@lillup.com>
2024-06-11 23:50:03 +0300

    Write tests for model and controller

--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -1,35 +1,35 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe SearchRecord, type: :model do
-#   # Create a valid search record for testing purposes
-#   let(:valid_search_record) { SearchRecord.new(query: 'example search', ip_address: '127.0.0.1') }
+RSpec.describe SearchRecord, type: :model do
+  # Create a valid search record for testing purposes
+  let(:valid_search_record) { SearchRecord.new(query: 'example search', ip_address: '127.0.0.1') }
 
-#   context 'validations' do
-#     it 'is valid with valid attributes' do
-#       expect(valid_search_record).to be_valid
-#     end
+  context 'validations' do
+    it 'is valid with valid attributes' do
+      expect(valid_search_record).to be_valid
+    end
 
-#     it 'is not valid without a query' do
-#       valid_search_record.query = nil
-#       expect(valid_search_record).not_to be_valid
-#       expect(valid_search_record.errors[:query]).to include("can't be blank")
-#     end
+    it 'is not valid without a query' do
+      valid_search_record.query = nil
+      expect(valid_search_record).not_to be_valid
+      expect(valid_search_record.errors[:query]).to include("can't be blank")
+    end
 
-#     it 'is not valid without an ip_address' do
-#       valid_search_record.ip_address = nil
-#       expect(valid_search_record).not_to be_valid
-#       expect(valid_search_record.errors[:ip_address]).to include("can't be blank")
-#     end
+    it 'is not valid without an ip_address' do
+      valid_search_record.ip_address = nil
+      expect(valid_search_record).not_to be_valid
+      expect(valid_search_record.errors[:ip_address]).to include("can't be blank")
+    end
 
-#     it 'is not valid if query is not unique for the same ip_address' do
-#       SearchRecord.create!(query: 'example search', ip_address: '127.0.0.1')
-#       expect(valid_search_record).not_to be_valid
-#       expect(valid_search_record.errors[:query]).to include('has already been taken')
-#     end
+    it 'is not valid if query is not unique for the same ip_address' do
+      SearchRecord.create!(query: 'example search', ip_address: '127.0.0.1')
+      expect(valid_search_record).not_to be_valid
+      expect(valid_search_record.errors[:query]).to include('has already been taken')
+    end
 
-#     it 'is valid if query is unique for a different ip_address' do
-#       SearchRecord.create!(query: 'example search', ip_address: '127.0.0.2')
-#       expect(valid_search_record).to be_valid
-#     end
-#   end
-# end
+    it 'is valid if query is unique for a different ip_address' do
+      SearchRecord.create!(query: 'example search', ip_address: '127.0.0.2')
+      expect(valid_search_record).to be_valid
+    end
+  end
+end
