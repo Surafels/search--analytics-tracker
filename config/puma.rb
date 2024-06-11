47a40d63ff81371576b26e42410e7a7dec34d6a2
Surafelfs <Surafel@lillup.com>
2024-06-11 20:41:00 +0300

    Update config for render

--- config/puma.rb
+++ config/puma.rb
@@ -27,6 +27,7 @@ port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "development" }
+# workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
